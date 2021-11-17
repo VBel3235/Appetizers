40c476fc6fad323b16239fe20c9b08fd2244dc85
VBel3235 <Vlad95.0753@gmail.com>
2021-11-17 11:15:23 +1000

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Владислав Белов on 17.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
